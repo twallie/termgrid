d9bbadd195e388bd4982ac80d80b566324a50308
twallie <thomaswallsmith@gmail.com>
2024-08-26 01:48:15 -0400

    feat: set() for TerminalGrid

--- src/grid.rs
+++ src/grid.rs
@@ -43,6 +43,22 @@ where
 
         Ok(value)
     }
+
+    pub fn set(&mut self, x: &usize, y: &usize, value: T) -> Result<(), GetError> {
+        let row = match self.vec.get_mut(*y) {
+            Some(v) => v,
+            None => return Err(GetError::RowOutOfBounds)
+        };
+
+        let cell = match row.get_mut(*x) {
+            Some(v) => v,
+            None => return Err(GetError::ColumnOutOfBounds)
+        };
+
+        *cell = value;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -51,6 +67,7 @@ mod tests {
 
     #[derive(Clone, PartialEq)]
     enum TestStates {
+        Filled,
         Empty
     }
 
@@ -67,6 +84,25 @@ mod tests {
         let grid = TerminalGrid::new(&TestStates::Empty).unwrap();
         match grid.get(&0, &0) {
             Ok(v) => assert!(v == &TestStates::Empty),
+            Err(_) => assert!(false)
+        }
+    }
+
+    #[test]
+    fn set_and_get() {
+        let mut grid = TerminalGrid::new(&TestStates::Empty).unwrap();
+        match grid.get(&0, &0) {
+            Ok(v) => assert!(v == &TestStates::Empty),
+            Err(_) => assert!(false)
+        }
+
+        match grid.set(&0, &0, TestStates::Filled) {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false)
+        }
+
+        match grid.get(&0, &0) {
+            Ok(v) => assert!(v == &TestStates::Filled),
             Err(_) => assert!(false)
         }
     }
